ccfe5ed3118d713df66cf9c10db628010f92bafb
Ke Bai <128360647+xjtluk@users.noreply.github.com>
2025-04-15 14:46:49 +0100

    1

---
+++ build/app/intermediates/cxx/Debug/5h6t53r4/logs/arm64-v8a/configure_command.bat
@@ -0,0 +1,20 @@
+@echo off
+"C:\\Users\\12714\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\12714\\Desktop\\casa0015\\flutter\\packages\\flutter_tools\\gradle\\src\\main\\groovy" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=arm64-v8a" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=arm64-v8a" ^
+  "-DANDROID_NDK=C:\\Users\\12714\\AppData\\Local\\Android\\sdk\\ndk\\26.3.11579264" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\12714\\AppData\\Local\\Android\\sdk\\ndk\\26.3.11579264" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\12714\\AppData\\Local\\Android\\sdk\\ndk\\26.3.11579264\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\12714\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\12714\\Desktop\\ms\\healthyair\\build\\app\\intermediates\\cxx\\Debug\\5h6t53r4\\obj\\arm64-v8a" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\12714\\Desktop\\ms\\healthyair\\build\\app\\intermediates\\cxx\\Debug\\5h6t53r4\\obj\\arm64-v8a" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BC:\\Users\\12714\\Desktop\\ms\\healthyair\\android\\app\\.cxx\\Debug\\5h6t53r4\\arm64-v8a" ^
+  -GNinja ^
+  -Wno-dev ^
+  --no-warn-unused-cli
